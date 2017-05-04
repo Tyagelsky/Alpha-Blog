140586393ff8b2513f5567cb77ea982506d3fee2
Yaroslav <tyagelsky@yandex.ru>
2017-05-05 02:50:52 +0300

    installed bootstrap gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article , only: [ :show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
@@ -19,15 +19,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "The article was edited"
       redirect_to article_path(@article)
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = "The article was destroyed"
     redirect_to articles_path
@@ -46,6 +46,11 @@ class ArticlesController < ApplicationController
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
