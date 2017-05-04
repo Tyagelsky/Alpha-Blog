267d9bf9fe4ebaf3be5133150cd9eae1c59a4dca
Yaroslav <tyagelsky@yandex.ru>
2017-05-04 18:40:17 +0300

    Added index action and view for it. Added some buttons for easiest movement in app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -26,7 +31,7 @@ class ArticlesController < ApplicationController
     if @article.update(article_params)
       flash[:notice] = "The article was edited"
       redirect_to article_path(@article)
-      
+
     else
       render 'edit'
     end
