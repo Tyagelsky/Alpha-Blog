cd216e06886cf283802d3a2c7d3775f2f0ab4c7c
Yaroslav <tyagelsky@yandex.ru>
2017-05-10 03:09:13 +0300

    add edit and update actions in the user controller and partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,21 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success]= "User account was succesfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+
+  end
+
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
